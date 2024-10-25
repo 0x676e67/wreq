e4346d69160079ec21aa063b780f27b7a709631c
0x676e67 <gngppz@gmail.com>
2024-10-25 12:42:19 +0800

    Update

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -1,9 +1,7 @@
 //! DNS resolution
 
 #[cfg(feature = "hickory-dns")]
-pub use hickory::HickoryDnsResolver;
-#[cfg(feature = "hickory-dns")]
-pub use hickory_resolver::config::LookupIpStrategy;
+pub use hickory::{HickoryDnsResolver, LookupIpStrategy};
 pub use resolve::{Addrs, Name, Resolve, Resolving};
 pub(crate) use resolve::{DnsResolverWithOverrides, DynResolver};
 
