6c02b202164c60844e9bb4dfbea618f2c09376ca
0x676e67 <gngppz@gmail.com>
2024-10-25 12:40:23 +0800

    refactor: Normalize DNS module exports

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -1,9 +1,13 @@
 //! DNS resolution
 
+#[cfg(feature = "hickory-dns")]
+pub use hickory::HickoryDnsResolver;
+#[cfg(feature = "hickory-dns")]
+pub use hickory_resolver::config::LookupIpStrategy;
 pub use resolve::{Addrs, Name, Resolve, Resolving};
 pub(crate) use resolve::{DnsResolverWithOverrides, DynResolver};
 
 pub(crate) mod gai;
 #[cfg(feature = "hickory-dns")]
-pub mod hickory;
+pub(crate) mod hickory;
 pub(crate) mod resolve;
