9163eebf1848697784a3c3b321511371979b6a86
0x676e67 <gngppz@gmail.com>
2024-12-06 23:15:04 +0800

    fix: Improve TLS connector creation, fix client creation taking too long

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -2,13 +2,24 @@ use rquest::tls::Impersonate;
 
 #[tokio::main]
 async fn main() -> Result<(), rquest::Error> {
+    let now = std::time::Instant::now();
     // Build a client to mimic Chrome131
-    let client = rquest::Client::builder()
+    let _client = rquest::Client::builder()
         .impersonate(Impersonate::Chrome131)
         .build()?;
+    println!("{:?}", now.elapsed());
 
-    // Use the API you're already familiar with
-    let resp = client.get("https://tls.peet.ws/api/all").send().await?;
+    let now = std::time::Instant::now();
+    // Build a client to mimic Chrome131
+    let _client2 = rquest::Client::builder()
+        .impersonate(Impersonate::Safari18)
+        .build()?;
+    println!("{:?}", now.elapsed());
+
+    let resp = _client.get("https://tls.peet.ws/api/all").send().await?;
+    println!("{}", resp.text().await?);
+
+    let resp = _client2.get("https://tls.peet.ws/api/all").send().await?;
     println!("{}", resp.text().await?);
 
     Ok(())
