1ef0806dfcd23dbde69f618827fca8264bad9c44
0x676e67 <gngppz@gmail.com>
2024-12-06 23:17:12 +0800

    Update examples

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -2,24 +2,24 @@ use rquest::tls::Impersonate;
 
 #[tokio::main]
 async fn main() -> Result<(), rquest::Error> {
-    let now = std::time::Instant::now();
     // Build a client to mimic Chrome131
-    let _client = rquest::Client::builder()
+    let now = tokio::time::Instant::now();
+    let client1 = rquest::Client::builder()
         .impersonate(Impersonate::Chrome131)
         .build()?;
-    println!("{:?}", now.elapsed());
+    println!("create client1 time: {:?}", now.elapsed());
 
-    let now = std::time::Instant::now();
     // Build a client to mimic Chrome131
-    let _client2 = rquest::Client::builder()
+    let now = tokio::time::Instant::now();
+    let client2 = rquest::Client::builder()
         .impersonate(Impersonate::Safari18)
         .build()?;
-    println!("{:?}", now.elapsed());
+    println!("create client2 time: {:?}", now.elapsed());
 
-    let resp = _client.get("https://tls.peet.ws/api/all").send().await?;
+    let resp = client1.get("https://tls.peet.ws/api/all").send().await?;
     println!("{}", resp.text().await?);
 
-    let resp = _client2.get("https://tls.peet.ws/api/all").send().await?;
+    let resp = client2.get("https://tls.peet.ws/api/all").send().await?;
     println!("{}", resp.text().await?);
 
     Ok(())
