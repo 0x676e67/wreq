8a90f9607a993dd4746734c65ae853480d4fd3e3
0x676e67 <gngppz@gmail.com>
2025-01-07 16:01:57 +0800

    update

--- src/client/http.rs
+++ src/client/http.rs
@@ -299,9 +299,11 @@ impl ClientBuilder {
         std::mem::swap(&mut self.config.tls, &mut settings.tls);
 
         // Set the http2 preference
-        if let Some(http2) = settings.http2 { self.config
+        if let Some(http2) = settings.http2 {
+            self.config
                 .builder
-                .with_http2_builder(|builder| apply_http2_settings(builder, http2)); }
+                .with_http2_builder(|builder| apply_http2_settings(builder, http2));
+        }
 
         self
     }
@@ -1556,9 +1558,11 @@ impl Client {
         inner.hyper.with_connector(|c| c.set_connector(connector));
 
         // Set the http2 preference
-        if let Some(http2) = settings.http2 { inner
+        if let Some(http2) = settings.http2 {
+            inner
                 .hyper
-                .with_http2_builder(|builder| apply_http2_settings(builder, http2)); }
+                .with_http2_builder(|builder| apply_http2_settings(builder, http2));
+        }
 
         Ok(())
     }
