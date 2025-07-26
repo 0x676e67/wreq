404c57b1c58e03d13747266b608993249e70a03e
0x676e67 <gngppz@gmail.com>
2025-07-26 08:51:08 +0800

    init

--- examples/request_with_proxy.rs
+++ examples/request_with_proxy.rs
@@ -1,9 +1,4 @@
-use wreq::{
-    Client,
-    http2::{
-        Http2Options, PseudoId, PseudoOrder, SettingId, SettingsOrder, StreamDependency, StreamId,
-    },
-};
+use wreq::{Client, Proxy};
 
 #[tokio::main]
 async fn main() -> wreq::Result<()> {
@@ -11,56 +6,13 @@ async fn main() -> wreq::Result<()> {
         .with_max_level(tracing::Level::TRACE)
         .init();
 
-    // HTTP/2 headers frame pseudo-header order
-    let headers_pseudo_order = PseudoOrder::builder()
-        .extend([
-            PseudoId::Method,
-            PseudoId::Path,
-            PseudoId::Authority,
-            PseudoId::Scheme,
-        ])
-        .build();
-
-    // HTTP/2 settings frame order
-    let settings_order = SettingsOrder::builder()
-        .extend([
-            SettingId::HeaderTableSize,
-            SettingId::EnablePush,
-            SettingId::MaxConcurrentStreams,
-            SettingId::InitialWindowSize,
-            SettingId::MaxFrameSize,
-            SettingId::MaxHeaderListSize,
-            SettingId::EnableConnectProtocol,
-            SettingId::NoRfc7540Priorities,
-        ])
-        .build();
-
-    let http2 = Http2Options::builder()
-        .header_table_size(65536)
-        .enable_push(false)
-        .initial_window_size(131072)
-        .max_frame_size(16384)
-        .initial_connection_window_size(12517377 + 65535)
-        .headers_stream_dependency(StreamDependency::new(StreamId::ZERO, 41, false))
-        .headers_pseudo_order(headers_pseudo_order)
-        .settings_order(settings_order)
-        .build();
-
-    // Build a client with emulation config
-    let client = Client::builder().cert_verification(false).build()?;
-
     // Use the API you're already familiar with
-    let resp = client
+    let resp = Client::new()
         .get("https://api.ip.sb/ip")
-        .emulation(http2)
+        .proxy(Proxy::all("socks5h://localhost:6153")?)
         .send()
         .await?;
-    let text = resp.text().await?;
-    println!("Response: {}", text);
-
-    let resp = client.get("https://api.ip.sb/ip").send().await?;
-    let text = resp.text().await?;
-    println!("Response: {}", text);
+    println!("{}", resp.text().await?);
 
     Ok(())
 }
