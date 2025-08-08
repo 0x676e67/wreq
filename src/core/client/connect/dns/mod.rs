5f206a81a8b4972a67e3176471233d61fc3fa9e2
0x676e67 <gngppz@gmail.com>
2025-08-08 08:25:15 +0800

    refactor(dns): consolidate legacy DNS modules

--- src/core/client/connect/dns/mod.rs
+++ src/core/client/connect/dns/mod.rs
@@ -1,25 +1,4 @@
-//! DNS Resolution used by the `HttpConnector`.
-//!
-//! This module contains:
-//!
-//! - A [`GaiResolver`] that is the default resolver for the `HttpConnector`.
-//! - The `Name` type used as an argument to custom resolvers.
-//!
-//! # Resolvers are `Service`s
-//!
-//! A resolver is just a
-//! `Service<Name, Response = impl Iterator<Item = SocketAddr>>`.
-//!
-//! A simple resolver that ignores the name and always returns a specific
-//! address:
-//!
-//! ```rust,ignore
-//! use std::{convert::Infallible, iter, net::SocketAddr};
-//!
-//! let resolver = tower::service_fn(|_name| async {
-//!     Ok::<_, Infallible>(iter::once(SocketAddr::from(([127, 0, 0, 1], 8080))))
-//! });
-//! ```
+//! DNS resolution
 
 pub(crate) mod gai;
 #[cfg(feature = "hickory-dns")]
