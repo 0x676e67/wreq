6e6091aae9d6fd044a7f0ee3ece16af8ef764702
0x676e67 <gngppz@gmail.com>
2025-08-16 01:59:15 +0800

    fmt

--- src/core/client/connect/uds.rs
+++ src/core/client/connect/uds.rs
@@ -6,20 +6,21 @@ use std::{
 };
 
 use http::Uri;
-use pin_project_lite::pin_project;
 use tokio::net::UnixStream;
 
 use super::{Connected, Connection};
 use crate::core::rt::TokioIo;
 
 pub type UnixConnectOptions = Option<Arc<Path>>;
+type ConnectResult = Result<TokioIo<UnixStream>, std::io::Error>;
+type BoxConnecting = Pin<Box<dyn Future<Output = ConnectResult> + Send>>;
 
 pub struct UnixConnector(pub Arc<Path>);
 
 impl tower::Service<Uri> for UnixConnector {
     type Response = TokioIo<UnixStream>;
     type Error = std::io::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = BoxConnecting;
 
     #[inline]
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
@@ -32,22 +33,6 @@ impl tower::Service<Uri> for UnixConnector {
             let io = fut.await?;
             Ok::<_, std::io::Error>(TokioIo::new(io))
         })
-    }
-}
-
-type ConnectResult = Result<TokioIo<UnixStream>, std::io::Error>;
-type BoxConnecting = Pin<Box<dyn Future<Output = ConnectResult> + Send>>;
-
-pin_project! {
-    // Not publicly exported (so missing_docs doesn't trigger).
-    //
-    // We return this `Future` instead of the `Pin<Box<dyn Future>>` directly
-    // so that users don't rely on it fitting in a `Pin<Box<dyn Future>>` slot
-    // (and thus we can change the type in the future).
-    #[must_use = "futures do nothing unless polled"]
-    pub struct UnixConnecting {
-        #[pin]
-        fut: BoxConnecting,
     }
 }
 
