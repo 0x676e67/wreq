66d0dae41b48a559e3a6bc0dcd14c6c6be6a81db
0x676e67 <gngppz@gmail.com>
2025-08-16 01:51:11 +0800

    add example

---
+++ examples/unix_socket.rs
@@ -0,0 +1,28 @@
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> wreq::Result<()> {
+    // Build a client
+    let client = wreq::Client::builder()
+        // Specify the Unix socket path
+        .unix_socket("/var/run/docker.sock")
+        .timeout(Duration::from_secs(10))
+        .build()?;
+
+    // Use the API you're already familiar with
+    let resp = client
+        .get("http://localhost/v1.41/containers/json")
+        .send()
+        .await?;
+    println!("{}", resp.text().await?);
+
+    // Or specify the Unix socket directly in the request
+    let resp = client
+        .get("http://localhost/v1.41/containers/json")
+        .unix_socket("/var/run/docker.sock")
+        .send()
+        .await?;
+    println!("{}", resp.text().await?);
+
+    Ok(())
+}
